e08fdbe9b77d1d11bc2cdd6b9e341d14dc9f01be
npatel17Confed <nsp4898@gmail.com>
2020-06-21 17:36:56 -0400

    Added some styling and made some changes to the javascript file

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ function refreshTable() {
    document.getElementById('table').innerHTML = "";
    for (let i=0; i<data.length; i++) {
       document.getElementById('table').innerHTML += "<tr>" +
-                                                      "<th><img src='" + data[i][1] + "' height='50px' width='50px'></th>" +
-                                                      "<th>"+ data[i][2] +"</th>" +
-                                                      "<th>"+ data[i][3] +"</th>" +
-                                                      "<th>"+ data[i][4] +"</th>" +
-                                                      "<th>"+ data[i][5] +"</th>" +
-                                                      "<th id='delete-btn' onclick='deleteRow(" + data[i][0] + ")'>x</th>" +
+                                                      "<td><img src='" + data[i][1] + "' height='50px' width='50px'></td>" +
+                                                      "<td>"+ data[i][2] +"</td>" +
+                                                      "<td>"+ data[i][3] +"</td>" +
+                                                      "<td>"+ data[i][4] +"</td>" +
+                                                      "<td>"+ data[i][5] +"</td>" +
+                                                      "<td id='delete-btn' onclick='deleteRow(" + data[i][0] + ")'><img src='images/close.jpg' height='50px' width='50px'></td>" +
                                                     "</tr>";
    }
 }
@@ -26,11 +26,15 @@ function addRow(event) {
    var job = document.getElementById("job").value;
    var salary = document.getElementById("salary").value;
    if (photo === "" || firstName === "" || lastName === "" || job === "" || salary === "") {
-      alert("Please add all the fields.");
+      document.getElementById("error").innerHTML = "Please add all the required fields.";
    } else {
-      data.push([count++, photo, firstName, lastName, job, salary]);
-      console.log(photo);
+      arr = photo.split('\\');
+      console.log(arr);
+      photo_link = "images/" + arr[arr.length-1];
+      console.log(photo_link);
+      data.push([count++, photo_link, firstName, lastName, job, salary]);
       document.getElementById("form").reset();
+      document.getElementById("error").innerHTML = "";
    }
    refreshTable();
 }
